a929651e62205d4ef02d4f894f70292f34e20113
mchytailo <mchytailo@proffiz.com>
2020-08-31 19:25:01 +0300

    project skeleton, configuration

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {CustomHeader} from '../styles/CustomHeader';
+
+const App: FC = () => {
+    return (
+        <div>
+            <CustomHeader>Hello World!</CustomHeader>
+        </div>
+    );
+}
+
+export default App;
