2b3223a31800fe2dabf5e5651c42e7af8d120fb6
mchytailo <mchytailo@proffiz.com>
2020-09-10 20:45:52 +0300

    Add some contact styles

--- src/components/Contact/Contact.styled.ts
+++ src/components/Contact/Contact.styled.ts
@@ -2,7 +2,37 @@ import styled from "styled-components";
 
 export const ContactSection = styled.div`  
 
-    height: 100vh;
-background: #9EA69C 0% 0% no-repeat padding-box;
+    min-height: 100vh;
+    padding: 80px 100px;
+    background: #9EA69C;
 
+    .ao-form {
+        .ao-form-label, .ao-combo-label {
+            text-align: left;
+            font: normal normal bold 18px/24px OptimaNormal;
+            letter-spacing: 4.32px;
+            color: #fff;
+            text-transform: uppercase;
+        }
+        
+        .ao-column-inner{
+            padding: 0 60px 0 0;
+        }
+        
+        .ao-form-field {
+            height: 80px;
+        }
+    }
+    `
+export const ContactText = styled.div`  
+    text-align: left;
+    font: normal normal normal 32px/40px OptimaNormal;
+    letter-spacing: 0px;
+    color: #FFFFFF;
+    margin-bottom: 80px;
+    `
+export const ContactLine = styled.div`  
+    height: 1px;
+    background: #FFFFFF;
+    margin-bottom: 80px;
     `
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,37 +1,44 @@
 import React, {FC} from 'react';
-import {ContactSection} from "./Contact.styled";
+import {ContactLine, ContactSection, ContactText} from "./Contact.styled";
 
 const Contact: FC = () => {
     return (
         <ContactSection>
+            <ContactText>
+                Register below to receive more information about Parkhouse, and to schedule a private tour. Please share
+                the best way to reach you, and a member of our Sales Team will contact you soon.
+            </ContactText>
+
+            <ContactLine/>
+
             <script type="text/javascript" id="aoform-script-0ea0194e-db01-4ec2-81d1-4f0f8550f840:d-0001">
                 {
                     !function (o, t, e, a) {
-                    o._aoForms = o._aoForms || [], o._aoForms.push(a);
-                    var n = function () {
-                        var o = t.createElement(e);
-                        o.src = ("https:" == t.location.protocol ? "https://" : "http://") + "marketing.polarispacific.com/acton/content/form_embed.js", o.async = !0;
-                        for (var a = t.getElementsByTagName(e)[0], n = a.parentNode, c = document.getElementsByTagName("script"), r = !1, s = 0; s < c.length; s++) {
-                            if (c[s].getAttribute("src") == o.getAttribute("src")) r = !0;
-                        }
-                        r ? typeof (_aoFormLoader) != "undefined" ? _aoFormLoader.load({
-                            id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840:d-0001",
-                            accountId: "35347",
-                            domain: "marketing.polarispacific.com",
-                            isTemp: false,
-                            noStyle: false,
-                            prefill: false
-                        }) : "" : n.insertBefore(o, a)
-                    };
-                    window.attachEvent ? window.attachEvent("onload", n) : window.addEventListener("load", n, !1), n()
-                }(window, document, "script", {
-                    id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840",
-                    accountId: "35347",
-                    domain: "marketing.polarispacific.com",
-                    isTemp: false,
-                    noStyle: false,
-                    prefill: false
-                })
+                        o._aoForms = o._aoForms || [], o._aoForms.push(a);
+                        var n = function () {
+                            var o = t.createElement(e);
+                            o.src = ("https:" == t.location.protocol ? "https://" : "http://") + "marketing.polarispacific.com/acton/content/form_embed.js", o.async = !0;
+                            for (var a = t.getElementsByTagName(e)[0], n = a.parentNode, c = document.getElementsByTagName("script"), r = !1, s = 0; s < c.length; s++) {
+                                if (c[s].getAttribute("src") == o.getAttribute("src")) r = !0;
+                            }
+                            r ? typeof (_aoFormLoader) != "undefined" ? _aoFormLoader.load({
+                                id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840:d-0001",
+                                accountId: "35347",
+                                domain: "marketing.polarispacific.com",
+                                isTemp: false,
+                                noStyle: false,
+                                prefill: false
+                            }) : "" : n.insertBefore(o, a)
+                        };
+                        window.attachEvent ? window.attachEvent("onload", n) : window.addEventListener("load", n, !1), n()
+                    }(window, document, "script", {
+                        id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840",
+                        accountId: "35347",
+                        domain: "marketing.polarispacific.com",
+                        isTemp: false,
+                        noStyle: false,
+                        prefill: false
+                    })
                 }
             </script>
         </ContactSection>
