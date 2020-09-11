59bd6c9cb9775b6768b6c81217163461e87da03d
mchytailo <mchytailo@proffiz.com>
2020-09-11 15:13:25 +0300

    update contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -10,40 +10,8 @@ const Contact: FC = () => {
             </ContactText>
 
             <ContactLine/>
-            <script>var accountId = "35347";var
-                formId = "0ea0194e-db01-4ec2-81d1-4f0f8550f840";var
-                marketingDomain = "marketing.polarispacific.com";
-            </script>
 
-            <script type="text/javascript"
-                    src="//a11058.actonservice.com/cdnr/67/acton/attachment/11058/f-0083/1/-/-/-/-/WPCF7Script.js"></script>
-            <script type="text/javascript"
-                    id="aoform-script-0ea0194e-db01-4ec2-81d1-4f0f8550f840:d-0001">{!function (o, t, e, a) {
-                o._aoForms = o._aoForms || [], o._aoForms.push(a);
-                var n = function () {
-                    var o = t.createElement(e);
-                    o.src = ("https:" == t.location.protocol ? "https://" : "http://") + "marketing.polarispacific.com/acton/content/form_embed.js", o.async = !0;
-                    for (var a = t.getElementsByTagName(e)[0], n = a.parentNode, c = document.getElementsByTagName("script"), r = !1, s = 0; s < c.length; s++) {
-                        if (c[s].getAttribute("src") == o.getAttribute("src")) r = !0;
-                    }
-                    r ? typeof (_aoFormLoader) != "undefined" ? _aoFormLoader.load({
-                        id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840:d-0001",
-                        accountId: "35347",
-                        domain: "marketing.polarispacific.com",
-                        isTemp: false,
-                        noStyle: false,
-                        prefill: false
-                    }) : "" : n.insertBefore(o, a)
-                };
-                window.attachEvent ? window.attachEvent("onload", n) : window.addEventListener("load", n, !1), n()
-            }(window, document, "script", {
-                id: "0ea0194e-db01-4ec2-81d1-4f0f8550f840",
-                accountId: "35347",
-                domain: "marketing.polarispacific.com",
-                isTemp: false,
-                noStyle: false,
-                prefill: false
-            })}</script>
+            <script type="text/javascript" id="aoform-script-b2986ccc-0cf1-4346-ad56-3edd9a936817:d-0001">{!function(o,t,e,a){o._aoForms=o._aoForms||[],o._aoForms.push(a);var n=function(){var o=t.createElement(e);o.src=("https:"==t.location.protocol?"https://":"http://")+"marketing.polarispacific.com/acton/content/form_embed.js",o.async=!0;for(var a=t.getElementsByTagName(e)[0],n=a.parentNode,c=document.getElementsByTagName("script"),r=!1,s=0;s<c.length;s++){if(c[s].getAttribute("src")==o.getAttribute("src"))r=!0;}r?typeof(_aoFormLoader)!="undefined"?_aoFormLoader.load({id:"b2986ccc-0cf1-4346-ad56-3edd9a936817:d-0001",accountId:"35347",domain:"marketing.polarispacific.com",isTemp:false,noStyle:false,prefill:true}):"":n.insertBefore(o,a)};window.attachEvent?window.attachEvent("onload",n):window.addEventListener("load",n,!1),n()}(window,document,"script",{id:"b2986ccc-0cf1-4346-ad56-3edd9a936817",accountId:"35347",domain:"marketing.polarispacific.com",isTemp:false,noStyle:false,prefill:true})}</script>
         </ContactSection>
     )
 
